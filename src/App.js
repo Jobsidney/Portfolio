bbaf2e881172b501fb2c091e976f262e0e07e50c
Jobsidney <jobsidney67@gmail.com>
2022-07-25 14:28:14 +0300

    Home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import MainSection from './components/Main-Section/MainSection';
 import SideBar from './components/SideBar/SideBar';
 
 function App() {
   return (
     <div className="main-container">
       <SideBar/>
+      <MainSection/>
     </div>
   );
 }
