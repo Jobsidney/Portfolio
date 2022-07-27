bf42371163d2242da6142909fecdb106399284a2
Jobsidney <jobsidney67@gmail.com>
2022-07-27 10:54:31 +0300

    Form data populating from form to object

--- src/components/Contacts/ContactForm.js
+++ src/components/Contacts/ContactForm.js
@@ -1,23 +1,43 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function ContactForm() {
+  const [formData,setFormData]=useState({
+    name:'',
+    email:'',
+    WHO:'',
+    WHY:'',
+    message:''
+  })
+  function handleChange(event){
+    const name=event.target.name;
+    const value=event.target.value;
+
+    setFormData({
+      ...setFormData,
+      [name]:value,
+    })
+  }
+  const handleSubmit=()=>{
+    fetch('')
+  }
+
   return (
     <div className="contact-form">
         <form>
-            <input type="text" placeholder='Your Name' />
-            <input type="email" placeholder='Email Address' required />
-            <select name="" id="">
+            <input type="text" placeholder='Your Name' name='name' />
+            <input type="email" placeholder='Email Address' name='email' required />
+            <select  id="" name='WHO'>
                 <option value="Employer">Employer</option>
                 <option value="Fun">Fun</option>
                  <option value="Collaborator">Collaborator</option>
             </select>
             {/* <input type="text" placeholder='Subject' required /> */}
-            <select name="" id="">
+            <select name="WHY" id="">
                 <option value="Employer-me">Employ Me</option>
                 <option value="Fun">Feed Back</option>
                  <option value="Collabo-req">Collaboration Request</option>
             </select>
-            <textarea id="Mailcontent" placeholder='Type message here..' name="content" rows="4" cols="10"></textarea>
+            <textarea id="Mailcontent" placeholder='Type message here..' name="message" rows="4" cols="10"></textarea>
             <button className='btnSubmit' type="submit">send</button>
 
         </form>
